bb97d9a8f7814cba2b5b4fe79f6d72337b7bce77
vaclavlohr <github.com@brtnik.info>
2024-05-28 22:29:40 +0200

    v14
    Opravená tlačítka (GPIO), doplněny diody

--- aukce.py
+++ aukce.py
@@ -12,6 +12,39 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.patches import Rectangle
 
+from gpiozero import LED
+import time
+
+# Nastavení LED pinů
+led_pins = [5, 6, 13, 19, 26]
+
+# Vytvoření seznamu objektů LED
+leds = [LED(pin) for pin in led_pins]
+
+def led_on(index):
+    """ Zapne LED podle zadaného indexu. """
+    leds[index].on()
+
+def led_off(index):
+    """ Vypne LED podle zadaného indexu. """
+    leds[index].off()
+
+def all_leds_on():
+    """ Zapne všechny LED. """
+    for led in leds:
+        led.on()
+
+def all_leds_off():
+    """ Vypne všechny LED. """
+    for led in leds:
+        led.off()
+
+def test_leds():
+    """ Testovací funkce pro zapnutí a vypnutí všech LED. """
+    all_leds_on()
+    time.sleep(2)  # Pauza, aby byly LED viditelně zapnuté
+    all_leds_off()
+
 # Globální proměnné
 num_players = 5
 num_auctions = 10  # Počet aukcí
@@ -25,6 +58,14 @@ registration_time_limit = 10  # čas na přihlášení v sekundách
 auction_active = False
 registration_active = True  # Inicializace stavu registrace
 auction_countdown = False
+
+registration_updated = False
+scores_updated = False
+update_labels = False
+price_label_text = ""
+round_label_text = ""
+countdown_label_text = ""
+
 
 # Celkový součet
 total_sum = 5000
@@ -52,16 +93,19 @@ print("Součet hodnot:", final_values.sum())
 
 
 def countdown_timer(count, action):
+    global countdown_label_text, update_labels
     for i in range(count, 0, -1):
-        countdown_label.config(text=f"{action} za {i} sekund.")
+        countdown_label_text = f"{action} za {i} sekund."
+        update_labels = True
         update_window()
         sleep(1)
-    #countdown_label.config(text=f"{action} právě probíhá.")
-    countdown_label.config(text="")
+    countdown_label_text = ""
+    update_labels = True
     update_window()
 
 def run_auction():
     global current_price, auction_active, current_auction, final_values, auction_countdown
+    global price_label_text, round_label_text, countdown_label_text, update_labels
     #current_price = 1000
     current_price = final_values[current_auction]
     bid_decrement = np.random.randint(2, 11)
@@ -69,8 +113,9 @@ def run_auction():
 
     #countdown_timer(5, "auction round")  # 5 sekundový odpočet před začátkem aukce
 
-    price_label.config(text=f"Startovní cena: {current_price}\n")
-    round_label.config(text=f"Kolo aukce #{current_auction+1} z {num_auctions}")
+    price_label_text=f"Startovní cena: {current_price}\n"
+    round_label_text=f"Kolo aukce #{current_auction+1} z {num_auctions}"
+    update_labels = True
     draw_gauge(current_price)
     update_window()
 
@@ -84,7 +129,8 @@ def run_auction():
 
         while auction_active and current_price > 0:
             current_price -= bid_decrement
-            price_label.config(text=f"Cena: {current_price}\n")
+            price_label_text=f"Cena: {current_price}\n"
+            update_labels = True
             draw_gauge(current_price)
             update_window()
             #sleep(0.01)  # Zkrácený čas pro dynamickou aukci
@@ -93,6 +139,7 @@ def run_auction():
             handle_auction_end(-1) #No winner
 
 def evaluate_winner(final):
+    global price_label_text, round_label_text, countdown_label_text, update_labels
     max_wins = -1
     min_score = float('inf')
     winner_index = -1
@@ -110,9 +157,10 @@ def evaluate_winner(final):
 
     # Aktualizace labelu s informací o vítězi
     if (final == False):
-        round_label.config(text=f"Průběžným vítězem aukce je: Hráč {winner_index + 1} s {win_rounds[winner_index]} výhrami a skóre {scores[winner_index]}")
+        round_label_text=f"Průběžným vítězem aukce je: Hráč {winner_index + 1} s {win_rounds[winner_index]} výhrami a skóre {scores[winner_index]}"
     if (final == True):
-        round_label.config(text=f"Celkovým vítězem aukce je: Hráč {winner_index + 1} s {win_rounds[winner_index]} výhrami a skóre {scores[winner_index]}")
+        round_label_text=f"Celkovým vítězem aukce je: Hráč {winner_index + 1} s {win_rounds[winner_index]} výhrami a skóre {scores[winner_index]}"
+    update_labels = True
     
     # Zvýraznění vítěze v Treeview
     for child in tree.get_children():
@@ -123,11 +171,13 @@ def evaluate_winner(final):
 
 def handle_auction_end(winner):
     global current_auction, auction_active
+    global price_label_text, round_label_text, countdown_label_text, update_labels
     if (winner >= 0):
-        price_label.config(text=f"{current_auction+1}. kolo aukce skončilo.\nVítězem kola se stal/a hráč/ka č. {winner+1}.")
+        price_label_text = f"{current_auction+1}. kolo aukce skončilo.\nVítězem kola se stal/a hráč/ka č. {winner+1}."
     else:
-        price_label.config(text=f"{current_auction+1}. kolo aukce skončilo.\nKolo nemá vítěze.")        
-    update_window()
+        price_label_text =f"{current_auction+1}. kolo aukce skončilo.\nKolo nemá vítěze."
+    update_labels = True
+    #update_window()
     current_auction += 1
     auction_active = False
     if current_auction < num_auctions:
@@ -136,18 +186,22 @@ def handle_auction_end(winner):
         run_auction()
     else:
         evaluate_winner(True)
-        countdown_label.config(text="Všechna kola aukce skončila.")
+        countdown_label_text = "Všechna kola aukce skončila."
         print("Všechna kola aukce skončila.")
         current_auction = 0  # Reset pro další kolo aukcí
+    update_labels = True
 
 def player_action(player_index):
+    print(f"Hráč {player_index} stiskl.")
     global auction_active
     if auction_active and registered_players[player_index]:
         auction_active = False
         scores[player_index] += current_price
         win_rounds[player_index] += 1
-        update_table()
+        #update_table()
         handle_auction_end(player_index)  # Řízení konce aukce a možné spuštění nové aukce
+        global scores_updated
+        scores_updated = True
 
 def start_auction():
     global registration_active, current_auction
@@ -161,14 +215,16 @@ def start_auction():
 
 def register_players():
     global registration_active
+    global price_label_text, round_label_text, countdown_label_text, update_labels
     registration_active = True
-    price_label.config(text="Probíhá registrace do hry.\nChcete-li hrát, stiskněte tlačítko.")
-    round_label.config(text="")
-    countdown_label.config(text="Stiskněte tlačítko a registrujte se do hry.")
+    price_label_text="Probíhá registrace do hry.\nChcete-li hrát, stiskněte tlačítko."
+    round_label_text=""
+    countdown_label_text="Stiskněte tlačítko a registrujte se do hry."
+    update_labels = True
     while sum(registered_players) < 2:
         update_window()
         sleep(0.1)
-    price_label.config(text="Probíhá registrace do hry.\nPoslední možnost se přihlásit.")
+    price_label_text="Probíhá registrace do hry.\nPoslední možnost se přihlásit."
     start_button.config(state=tk.NORMAL)  # Enable start button after registration of 2 players
     countdown_timer(10, "Registrace hráčů končí")  # 20 sekundový odpočet před začátkem dalsi hry
     if registration_active:
@@ -193,28 +249,61 @@ def reset_game():
     start_button.config(state=tk.DISABLED)
     register_players()
 
-def button_pressed(button):
-    player_index = buttons.index(button)
+def button_pressed(button, index):
+    """Funkce reaguje na stisk tlačítka přes GPIO."""
+    player_index = buttons.index(button)  # Získání indexu tlačítka
     if registration_active:
         registered_players[player_index] = True
-        #player_labels[player_index].config(text=f"Player {player_index + 1} (Registered): {scores[player_index]}")
-        #player_labels[player_index].config(text=f"Hráč č. {player_index + 1} registrován.")
-        print(f"Hráč č. {player_index + 1} registrován.")
+        print(f"Hráč č. {player_index + 1} registrován tlačítkem.")
+        global registration_updated
+        registration_updated = True
     elif auction_active:
         player_action(player_index)
+    #print(f"Tlačítko {index + 1} bylo stisknuto hráčem {player_index + 1}.")
 
-for button in buttons:
-    button.when_pressed = button_pressed
+        
+
+# Přiřazení funkce k tlačítku, předpokládáme že `buttons` je seznam Button objektů z gpiozero
+for i, button in enumerate(buttons):
+    button.when_pressed = lambda button=button, i=i: button_pressed(button, i)
 
 def handle_key_press(event, player_index):
     if registration_active:
         registered_players[player_index] = True
-        update_table()
-        #player_labels[player_index].config(text=f"Hráč č. {player_index + 1} registrován: {scores[player_index]}")
-        print(f"Hráč č. {player_index + 1} registrován.")
+        #update_table()
+        print(f"Hráč č. {player_index + 1} registrován klávesnicí.")
+        global registration_updated
+        registration_updated = True
     elif auction_active:
         player_action(player_index)
-        
+
+
+def update_table():
+    global scores, win_rounds, registered_players, num_players
+    for i in range(num_players):
+        if registered_players[i]:
+            window.after(0, lambda i=i: tree.item(i, values=(f"Hráč {i + 1}", scores[i], win_rounds[i])))
+        else:
+            window.after(0, lambda i=i: tree.item(i, values=(f"Hráč {i + 1}", "nehraje", "")))
+    pass
+
+def periodic_check():
+    global registration_updated, scores_updated, update_labels
+    global price_label_text, round_label_text, countdown_label_text
+    if registration_updated:
+        update_table()
+        registration_updated = False
+    if scores_updated:
+        update_table()
+        scores_updated = False
+    if update_labels:
+        update_table()
+        price_label.config(text=price_label_text)
+        round_label.config(text=round_label_text)
+        countdown_label.config(text=countdown_label_text)
+        update_labels = False
+    update_window()
+    window.after(100, periodic_check)
 
 #dekorátor pro exception při zavírání okna
 def catch_tcl_error(func):
@@ -229,11 +318,29 @@ def catch_tcl_error(func):
 @catch_tcl_error
 def update_window():
     window.update()
+    pass
 
 
 # GUI setup
 window = tk.Tk()
+window.after(100, periodic_check)
 window.title("Dutch Auction Game")
+window.wm_attributes('-fullscreen', 'true')  # Otevře okno v režimu celé obrazovky
+
+# Nastavení min. šířky sloupců
+#window.columnconfigure(0, weight=1)
+#window.columnconfigure(1, weight=1)
+#window.columnconfigure(2, weight=1)
+
+# Vytvoření tří widgetů Label, jeden pro každý sloupec
+#label1 = tk.Label(window, text="Sloupec 1", bg="red")
+#label1.grid(row=0, column=0, sticky="nsew")  # 'nsew' roztažení na všechny strany
+
+#label2 = tk.Label(window, text="Sloupec 2", bg="green")
+#label2.grid(row=0, column=1, sticky="nsew")
+
+#label3 = tk.Label(window, text="Sloupec 3", bg="blue")
+#label3.grid(row=0, column=2, sticky="nsew")
 
 
 # Gauge plot setup
@@ -270,22 +377,28 @@ def draw_gauge(value, max_value=1500):
 draw_gauge(1500)  # Startovní hodnota gauge
 
 price_label = tk.Label(window, text=f"Celková cena: {total_sum}\n", font=('Helvetica', 18))
+#price_label.grid(row=0, column=1)
 price_label.pack(pady=20)
 
 round_label = tk.Label(window, text="", font=('Helvetica', 16))
+#round_label.grid(row=0, column=1)
 round_label.pack(pady=20)
 
 countdown_label = tk.Label(window, text="Stiskněte tlačítko a registrujte se do hry.", font=('Helvetica', 16))
+#countdown_label.grid(row=0, column=1)
 countdown_label.pack(pady=10)
 
 
 style = ttk.Style()
-style.theme_use('clam')  # Použijeme téma 'clam', které umožňuje úpravy
+style.theme_use('clam')  # Použijeme téma 'clam', které umožňuje úpravy // alt, default, classic, clam, aqua
 
 # Definování stylů pro normální a vítězné řádky
 style.map('Treeview', background=[('selected', 'blue')])
 style.configure('Treeview', background='white', fieldbackground='white')
 style.configure('Winner.Treeview', background='gold')
+style.configure("Treeview", font=('Helvetica', 14))  # Nastavení velikosti písma
+style.configure("Treeview.Heading", font=('Helvetica', 18))  # Zvětšení písma pro záhlaví
+style.configure("Treeview.Rowheight", rowheight=10)  # Nastavení výšky řádku
 
 # Vytvoření Treeview widgetu
 tree = ttk.Treeview(window, columns=("Player", "Score", "Wins"), show="headings", height=5)
@@ -297,23 +410,30 @@ tree.column("Wins", anchor=tk.E) #zarovnání doprava
 # Nastavení tagu a stylu pro vítěze
 tree.tag_configure('normal', background='white')  # Normální pozadí
 tree.tag_configure('winner', background='gold')   # Zvýrazněné pozadí pro vítěze
+
+def adjust_row_height(tree, height=30):
+    """Nastaví výšku řádku Treeview vytvořením transparentního obrázku s požadovanou výškou."""
+    # Vytvoření transparentního obrázku o požadované výšce
+    img = tk.PhotoImage(height=height, width=1)
+    tree.img = img  # Uložení obrázku, aby nebyl odstraněn garbage collectorem
+    for item in tree.get_children():
+        tree.item(item, image=img)  # Přiřazení obrázku k položkám
+# Nastavení výšky řádku
+adjust_row_height(tree)
+
+#tree.grid(row=0, column=1)
 tree.pack(pady=20)
 
 # Inicializace tabulky s hráči
 for i in range(num_players):
     tree.insert("", "end", iid=i, values=(f"Hráč {i + 1}", scores[i], win_rounds[i]))
 
-def update_table():
-    for i in range(num_players):
-        if (registered_players[i]):
-            tree.item(i, values=(f"Hráč {i + 1}", scores[i], win_rounds[i]))
-        else:
-            tree.item(i, values=(f"Hráč {i + 1}", "nehraje", ""))
-
 start_button = tk.Button(window, text="Zahájit hru", command=start_auction, state=tk.DISABLED)
+#start_button.grid(row=0, column=1)
 start_button.pack(pady=20)
 
 reset_button = tk.Button(window, text="Resetovat hru", command=reset_game)
+#reset_button.grid(row=0, column=1)
 reset_button.pack(pady=10)
 
 # Klávesnicové zkratky pro hráče a startovací tlačítko
@@ -328,6 +448,11 @@ window.bind('<q>', lambda e: window.quit())  # Bind 'q' key to quit
 #app = GaugeAnimation(window)
 
 update_table()
+
+
+# Testování
+test_leds()
+
 register_players()  # Start registration process
 
 window.mainloop()
